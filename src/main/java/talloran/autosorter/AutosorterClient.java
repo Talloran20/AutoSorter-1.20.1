fd38264e1259fce01a4fb8c0de5108aa85519186
Tarasov Borislav <borislav.tarasov02@bk.ru>
2023-10-07 18:05:33 +0500

    Initial commit

---
+++ src/main/java/talloran/autosorter/AutosorterClient.java
@@ -0,0 +1,10 @@
+package talloran.autosorter;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AutosorterClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/talloran/autosorter/AutosorterDataGenerator.java
@@ -0,0 +1,11 @@
+package talloran.autosorter;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AutosorterDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
